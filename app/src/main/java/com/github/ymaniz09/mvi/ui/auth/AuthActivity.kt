a40ad693b96a99a31001b407bf6f161a4ba611b3
Ygor <ygor.santos@thoughtworks.com>
2020-05-08 16:38:53 -0300

    first commit

---
+++ app/src/main/java/com/github/ymaniz09/mvi/ui/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.github.ymaniz09.mvi.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.ymaniz09.mvi.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
